4317e3770e08b71b31f0d2a7667c7f17eda5ecb9
Razak <moabdulrazak17@gmail.com>
2023-04-12 01:29:24 +0000

    feat(components): create navbar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import Wrapper from "../Wrapper";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+        py-4
+        border-b-[1px]
+        "
+      >
+        <Wrapper>
+          <div
+            className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Wrapper>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
